c01f82acffcf6b1bf98983139c696d5e6574abc2
Phani Adabala <38834236+adabalap@users.noreply.github.com>
2025-04-29 01:06:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('boots-converter-cache').then((cache) => {
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/sw.js',
-        '/boots.png',
-        '/icon-192.png',
-        '/icon-512.png'
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
