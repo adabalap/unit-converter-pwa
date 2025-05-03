448d6532db857b017927601de85bca728437d310
Phani Adabala <38834236+adabalap@users.noreply.github.com>
2025-05-04 00:03:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch',e=>{});
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('boots-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json',
+        '/icons/boots.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
